17b072a1a454e459ecaacdd0b18b8158b5d75a58
FLUVI <79911021+FLUVI@users.noreply.github.com>
2021-03-02 18:08:58 +0100

    Update FLUVIXD.py

--- FLUVIXD.py
+++ FLUVIXD.py
@@ -4,18 +4,16 @@ import sys
 os.system("clear")
 
 def XD():
-   os.system("cd sherlock","python3 -m pip install -r requirements.txt")
-   print("Instalado correctamente")
-   
-   
+	os.system("cd sherlock","pip3 install -r requeriments.txt")
+	print("requeriments sucesfully instaled!!!")
+
 while True:
-  print("que quieres hacer?")
-  opcion = input("\n-1. instalar sherlock\n-2. salir\n")
-  
-  if opcion == '1':
-      XD()
-      
-  else:
-  
-      print("saliendo...")
-      sys.exit()
+	print("Buenas mi pequeno skid que elegiras el dia de hoy?")
+	opcion = input("\n-1. Instalar sherlock\n-2. Salir\n")
+
+	if opcion == '1':
+		XD()
+
+	else:
+		print("Saliendo...")
+		sys.exit()
